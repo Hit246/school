104925f47737678e2846dd8687cbc6ba8ebecf10
Hit246 <hitarth0236@gmail.com>
2026-01-03 15:04:57 +0530

    live backend added

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const API_URL = import.meta.env.PROD 
+  ? 'https://school-2j95.onrender.com/api'
+  : 'http://localhost:5000/api';
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+import { API_URL } from '../config';
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem('adminToken');
